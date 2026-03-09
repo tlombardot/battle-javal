98ea75587c7bfdeb4d9a4a487238f4a7d0e841a3
Thomas Lombardot <thomas.lombardot@gmail.com>
2026-03-09 14:52:38 +0100

    Feat : Init Log UI & Select UI
    Co-authored-by: Darill. <darylleinstein@gmail.com>
    Co-authored-by: Louis269 <Louis269@users.noreply.github.com>
    Signed-off-by: Thomas Lombardot <thomas.lombardot@gmail.com>

---
+++ src/main/java/school/coda/darill_thomas_louis/bataillejavale/ui/LogGameUI.java
@@ -0,0 +1,9 @@
+package school.coda.darill_thomas_louis.bataillejavale.ui;
+
+import javafx.scene.layout.GridPane;
+
+public class LogGameUI extends GridPane {
+    public LogGameUI() {
+        // TODO document why this constructor is empty
+    }
+}
---
+++ src/main/java/school/coda/darill_thomas_louis/bataillejavale/ui/SelectBoard.java
@@ -0,0 +1,7 @@
+package school.coda.darill_thomas_louis.bataillejavale.ui;
+
+public class SelectBoard {
+    public SelectBoard() {
+        // TODO document why this constructor is empty
+    }
+}
