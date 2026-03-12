88736101364f3ec0131f557730d0c431da4f2e5b
Thomas Lombardot <thomas.lombardot@gmail.com>
2026-03-12 12:23:07 +0100

    Feat : Add package-info
    Co-authored-by: Darill. <darylleinstein@gmail.com>
    Co-authored-by: Louis269 <Louis269@users.noreply.github.com>
    Signed-off-by: Thomas Lombardot <thomas.lombardot@gmail.com>

---
+++ src/main/java/school/coda/darill_thomas_louis/bataillejavale/core/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Logique-métier de la bataille navale
+ * Règle de placement des vaisseaux
+ * Mécanique de tirs
+ * Règle de fin de partie
+ */
+package school.coda.darill_thomas_louis.bataillejavale.core;
---
+++ src/main/java/school/coda/darill_thomas_louis/bataillejavale/ui/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Foncrionnalité relatif à l'interface utilisateur
+ */
+package school.coda.darill_thomas_louis.bataillejavale.ui;
